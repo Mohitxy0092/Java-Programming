be130a17356978b6218a0cef59998b60912ca695
Mohitxy0092 <mohit952096@gmail.com>
2025-12-09 16:25:12 +0530

    added code

---
+++ 6-Conditional_Statements/codes/conditions.java
@@ -0,0 +1,10 @@
+public class conditions {
+  public static void main(String[] args) {
+    int age = 99;
+    if (age >= 18) {
+      System.out.println("You are an adult");
+    } else {
+      System.out.println("You are a minor");
+    }
+  }
+}
