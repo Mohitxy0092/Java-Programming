024ed071057a485bb6edb37ea80756a45f714827
Mohitxy0092 <mohit952096@gmail.com>
2026-07-04 18:03:46 +0530

    Fixed Package code.

---
+++ 17-Packages/codes/mypack1/inner/Demo2.java
@@ -0,0 +1,7 @@
+package mypack1.inner;
+
+public class Demo2 {
+    public void display() {
+      System.out.println("Hello from demo2..");
+}
+}
---
+++ 17-Packages/codes/mypack1/myMath/Add.java
@@ -0,0 +1,15 @@
+package mypack1.myMath;
+
+public class Add {
+    public int addition(int var1, int var2) {
+      return var1 + var2;
+   }
+
+   public float addition(float var1, float var2) {
+      return var1 + var2;
+   }
+
+   public double addition(double var1, double var3) {
+      return var1 + var3;
+   }
+}
